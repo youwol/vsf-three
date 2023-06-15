63fb7594a43575e8cfdc339e6bba4d0762c85cd5
youwol-guillaume <greinisch@youwol.com>
2023-06-15 12:10:53 +0200

    📝 update global documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /**
- * Visual Studio Flow toolbox wrapping the [three.js](https://threejs.org/) library.
+ * Visual Studio Flow toolbox wrapping the <a href="https://threejs.org/" target="_blank">three.js</a> library.
  *
  * For comprehensive information about the Visual Studio Flow (vs-flow) environment as a whole, a user guide can be
  * accessed <a href="https://l.youwol.com/doc/@youwol/vs-flow" target="_blank" >here</a>.
  * For developers seeking detailed documentation, please refer to the resources available
- *  <a href="https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/vs-flow&tab=doc" target="_blank" >here</a>
+ *  <a href="https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/vs-flow&tab=doc" target="_blank" >here</a>.
  *
  * We encourage you to participate in the development process of this toolbox by opening issues or contributing to the
  * project to add new modules and features. Your feedback and contributions are highly appreciated.
