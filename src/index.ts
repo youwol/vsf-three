ea7d1869b6d9d26a2021239f029d23f3087bc572
youwol-guillaume <greinisch@youwol.com>
2023-06-15 10:25:22 +0200

    📝 Update overall documentation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 /**
  * Visual Studio Flow toolbox wrapping the [three.js](https://threejs.org/) library.
  *
- * For comprehensive information about the Visual Studio Flow (vs-flow) environment as a whole, a user guide can be accessed [here](https://l.youwol.com/doc/@youwol/vs-flow).
- * For developers seeking detailed documentation, please refer to the resources available [here](https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/vs-flow&tab=doc).
+ * For comprehensive information about the Visual Studio Flow (vs-flow) environment as a whole, a user guide can be
+ * accessed <a href="https://l.youwol.com/doc/@youwol/vs-flow" target="_blank" >here</a>.
+ * For developers seeking detailed documentation, please refer to the resources available
+ *  <a href="https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/vs-flow&tab=doc" target="_blank" >here</a>
  *
  * We encourage you to participate in the development process of this toolbox by opening issues or contributing to the
  * project to add new modules and features. Your feedback and contributions are highly appreciated.
  *
- * Typical example:
+ * Illustrative example:
  * <iframe id="iFrameExample" src="" width="100%" height="800px"></iframe>
  * <script>
  *     const src = `return async ({project, cell, env}) => {
