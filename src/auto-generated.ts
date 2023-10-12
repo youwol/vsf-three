ff534b2154dbd949dc261a836bc98223abaeef4f
youwol-guillaume <greinisch@youwol.com>
2023-10-12 12:42:52 +0200

    :arrow_up: upgrade `vsf-core` to `0.2.3`

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -1,7 +1,7 @@
 
 const runTimeDependencies = {
     "externals": {
-        "@youwol/vsf-core": "^0.2.0",
+        "@youwol/vsf-core": "^0.2.3",
         "rxjs": "^6.5.5",
         "three": "^0.152.0"
     },
